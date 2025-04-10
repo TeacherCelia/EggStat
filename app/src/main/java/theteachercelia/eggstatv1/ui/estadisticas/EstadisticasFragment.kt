bb6cebabd77649a6c76e95ebaf04c984e6bda828
TeacherCelia <theteachercelia@gmail.com>
2025-04-10 20:22:02 +0200

    Implementado fragment_estadisticas. Se añade libreria externa para visualizar gráficos, se conectan con Firebase para que se muestren y se hacen algunos arreglos para su mejor visualizacion.

--- app/src/main/java/theteachercelia/eggstatv1/ui/estadisticas/EstadisticasFragment.kt
+++ app/src/main/java/theteachercelia/eggstatv1/ui/estadisticas/EstadisticasFragment.kt
@@ -4,40 +4,119 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import theteachercelia.eggstatv1.databinding.FragmentEstadisticasBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import theteachercelia.eggstatv1.R
 
 class EstadisticasFragment : Fragment() {
-    // TODO: quitar binding
 
-    private var _binding: FragmentEstadisticasBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var viewModel: EstadisticasViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val estadisticasViewModel =
-            ViewModelProvider(this).get(EstadisticasViewModel::class.java)
-
-        _binding = FragmentEstadisticasBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        estadisticasViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return inflater.inflate(R.layout.fragment_estadisticas,container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //instancia del viewmodel
+        viewModel = ViewModelProvider(this)[EstadisticasViewModel::class.java]
+
+        //referencias a views
+        val graficoCircular = view.findViewById<PieChart>(R.id.pieChart_Gallinas)
+        val graficoBarras = view.findViewById<BarChart>(R.id.barChart_Equipos)
+
+        //observadores
+        viewModel.mapaGallinas.observe(viewLifecycleOwner) { mapa ->
+            actualizarPieChart(mapa, graficoCircular)
+        }
+
+        viewModel.mapaEquipos.observe(viewLifecycleOwner) { mapa ->
+            actualizarBarChart(mapa, graficoBarras)
+        }
+
+        // Lógica UI (listeners, etc)
+
+
+    }
+
+    //otros metodos
+    private fun actualizarPieChart(mapa: Map<String, Int>, pieChart: PieChart) {
+        //1-- convertir en datos de PieEntry el mapa de gallinas
+        val datosGallinas = mapa.map { (nombreGallina, totalHuevos) ->
+            PieEntry(totalHuevos.toFloat(), nombreGallina)
+        }
+
+        //2-- crear dataset que queremos representar
+        val dataSetGallinas = PieDataSet(datosGallinas, "Gallinas")
+
+        //3-- aplicar colores automáticamente
+        dataSetGallinas.setColors(*ColorTemplate.MATERIAL_COLORS)
+
+        //4--asociar datos al gráfico
+        pieChart.data = PieData(dataSetGallinas)
+
+        //5-- modificaciones visuales
+
+        // quitar descripcion por defecto
+        pieChart.description.isEnabled = false
+        pieChart.legend.isEnabled = false
+
+        pieChart.invalidate() //refrescar grafico
+        pieChart.requestLayout()// refrescar gráfico
+    }
+
+    private fun actualizarBarChart(mapa: Map<String, Int>, barChart: BarChart) {
+        //1-- convertir en datos de BarEntry el mapa de equipos
+        val datosEquipos = mapa.entries.mapIndexed { indice, (nombreEquipo, puntos) ->
+            BarEntry(indice.toFloat(), puntos.toFloat())
+        }
+
+        //2-- crear dataset que queremos representar
+        val dataSetEquipos = BarDataSet(datosEquipos, "Equipos")
+
+        //3-- aplicar colores automáticamente
+        dataSetEquipos.setColors(*ColorTemplate.MATERIAL_COLORS)
+
+        //4-- le ponemos los nombres de equipo abajo
+        val labels = mapa.keys.toList()
+
+        //5-- asociar datos al grafico
+        barChart.data = BarData(dataSetEquipos)
+
+        // eje X
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.xAxis.granularity = 1f
+        barChart.xAxis.setDrawGridLines(false)
+        barChart.xAxis.labelRotationAngle = -45f // inclina un poco
+
+        // quitar descripcion por defecto
+        barChart.description.isEnabled = false
+
+        // config de leyenda
+        barChart.legend.isEnabled = false
+
+        // refrescar grafico
+        barChart.invalidate()
+        // Espacio extra abajo para que los nombres no se corten
+        barChart.setExtraBottomOffset(40f)
+        barChart.requestLayout()
     }
 }
--- app/src/main/java/theteachercelia/eggstatv1/ui/estadisticas/EstadisticasViewModel.kt
+++ app/src/main/java/theteachercelia/eggstatv1/ui/estadisticas/EstadisticasViewModel.kt
@@ -3,12 +3,75 @@ package theteachercelia.eggstatv1.ui.estadisticas
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import theteachercelia.eggstatv1.bd.Equipo
+import theteachercelia.eggstatv1.bd.Gallina
 
 class EstadisticasViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Aqui aparecerán estadísticas en tiempo real de puntos, huevos, etc..."
+    // instanciamos firebase (auth y database)
+    private val firebaseDB = FirebaseDatabase.getInstance().reference
+
+    // livedata de los datos a visualizar
+    private val _mapaGallinas = MutableLiveData<Map<String,Int>>()
+    val mapaGallinas : LiveData<Map<String,Int>> = _mapaGallinas
+
+    private val _mapaEquipos = MutableLiveData<Map<String,Int>>()
+    val mapaEquipos : LiveData<Map<String,Int>> = _mapaEquipos
+
+    init {
+
+        cargarDatosEstadisticas()// o lo que necesites
     }
-    val text: LiveData<String> = _text
+
+    //--- carga de datos
+    private fun cargarDatosEstadisticas() { //cargamos los datos del livedata
+        val gallinasBD = firebaseDB.child("gallinas")
+        val equiposBD = firebaseDB.child("equipo")
+
+        // cargar datos de gallinas
+        gallinasBD.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //creamos el mapa de datos para el livedata
+                val mapGallina = mutableMapOf<String,Int>()
+                for (gallinaSnap in snapshot.children) {
+                    val nombre = gallinaSnap.child("nombre_gallina").getValue(String::class.java)
+                    val huevos = gallinaSnap.child("total_huevos").getValue(Int::class.java) ?: 0
+                    if (!nombre.isNullOrEmpty()) {
+                        mapGallina[nombre] = huevos
+                    }
+                    _mapaGallinas.value = mapGallina
+                }
+
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        // cargar datos de equipos
+        equiposBD.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val mapa = mutableMapOf<String, Int>()
+                for (equipoSnap in snapshot.children) {
+                    val nombre = equipoSnap.child("nombre_equipo").getValue(String::class.java)
+                    val puntos = equipoSnap.child("puntos_equipo").getValue(Int::class.java) ?: 0
+                    if (!nombre.isNullOrEmpty()) {
+                        mapa[nombre] = puntos
+                    }
+                }
+                _mapaEquipos.value = mapa
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+    }
+
+    //--- metodos extra si se necesitan
+
 
 }
